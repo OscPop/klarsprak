88f886debab492f3fe2f726aa3b457b950542ccc
Oscar Larsen <oscarpopp98@gmail.com>
2023-04-27 09:28:32 +0200

    renamed main -> tests and added functionality to app.py

--- app.py
+++ app.py
@@ -3,8 +3,25 @@ import os
 import streamlit as st
 import pandas as pd
 import numpy as np
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 st.title("Klarspråksmaskineriet")
+
+txt = st.text_area("Klistra in text som ska skrivas om till klarspråk: ",
+             placeholder="den her teksten er int klearsprok")
+
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": txt}
+  ]
+)
+
+
+st.write(completion.choices[0].message)
