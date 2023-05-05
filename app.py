f5dce1161c8e208f54a11bac1986a7e6a8092b24
Oscar Larsen <oscarpopp98@gmail.com>
2023-05-05 13:33:25 +0200

    added safety instructions

--- app.py
+++ app.py
@@ -54,8 +54,15 @@ def proc():
 
 
 st.title("Klarspråksmaskineriet")
-st.write("Det här är en prototyp för att skriva om text till klarspråk. Det är en del av projektet Klarspråksmaskineriet. \
-        All information går i nuläget via amerikanska molntjänster, så klistra INTE in känslig information i fältet nedan.")
+st.write("Det här är en prototyp för att skriva om text till klarspråk med hjälp av chatboten GPT från OpenAI. All information går i nuläget via amerikanska molntjänster, så klistra INTE in känslig information i fältet nedan.")
+st.write("Det finns många olika AI-modeller att välja på från OpenAI, här kan 2 olika användas: GPT-3.5-turbo och GPT-4.")
+st.write("GPT-3.5-turbo är lite snabbare men GPT-4 ska vara lite bättre.")
+st.write("Det går att läsa mer om modellerna här: https://openai.com/blog/gpt-3-5b/ och här: https://openai.com/blog/gpt-4/")
+
+st.write("Detta är en tidig testversion av en sorts klarspråksassistent som är under utveckling.")
+st.write("Den kan endast lämna förbättringsförslag på ren text och inte på layout eller bilder.")
+st.write("Det primära användningsområdet bedöms vara de som vill få inspiration på hur man kan skriva om text till klarspråk.")
+st.write("Läs alltid igenom texten som genereras med kritiska ögon och använd sunt förnuft. Det kan hända att AI:n använder en omformulering som ändrar innebörden av texten.")
 
 
 # Välja modell
@@ -67,10 +74,10 @@ model = st.selectbox("Här kan du välja vilken AI-modell som ska användas:", [
 
 # Skriva in prompt
 st.text_area("Klistra in text som ska skrivas om till klarspråk: ", 
-                      placeholder="den her teksten er int klearsprok", 
-                      height=600,
-                      on_change=proc,
-                      key="prompt")
+            placeholder="den her teksten er int klearsprok", 
+            height=600,
+            on_change=proc,
+            key="prompt")
 
 
 
