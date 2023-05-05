e42bde11b62ab4b4c938196023a6653c1499691e
Oscar Larsen <oscarpopp98@gmail.com>
2023-05-05 14:09:19 +0200

    updated test_branch

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = st.secrets["OPENAI_API_KEY"] #os.getenv("OPENAI_API_KEY")
 
 
 # Initiera session state
